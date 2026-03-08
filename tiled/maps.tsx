2547c54a450180eab8ef48112e882fadf45beec1
Sani <nitori@chireiden.net>
2026-03-08 01:26:05 +0100

    Prepare tiled map

---
+++ tiled/maps.tsx
@@ -0,0 +1,280 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="maps" tilewidth="6400" tileheight="3496" tilecount="92" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="1">
+  <image source="../html/maps/default/Modern_town.webp" width="3520" height="1897"/>
+ </tile>
+ <tile id="2">
+  <image source="../html/maps/default/Modern_Town_Barber.webp" width="704" height="463"/>
+ </tile>
+ <tile id="3">
+  <image source="../html/maps/default/Modern_Town_Guild_Hall.webp" width="928" height="543"/>
+ </tile>
+ <tile id="4">
+  <image source="../html/maps/default/moogle_map.webp" width="1280" height="738"/>
+ </tile>
+ <tile id="5">
+  <image source="../html/maps/default/North_Flower_Fields.webp" width="1920" height="991"/>
+ </tile>
+ <tile id="6">
+  <image source="../html/maps/default/North_Jab_stab.webp" width="1920" height="1071"/>
+ </tile>
+ <tile id="7">
+  <image source="../html/maps/default/North_of_Town.webp" width="1120" height="560"/>
+ </tile>
+ <tile id="8">
+  <image source="../html/maps/default/north_winter_house_inside.webp" width="1152" height="688"/>
+ </tile>
+ <tile id="9">
+  <image source="../html/maps/default/North_winter_town.webp" width="2176" height="1295"/>
+ </tile>
+ <tile id="10">
+  <image source="../html/maps/default/Ocean_Map.webp" width="3200" height="1600"/>
+ </tile>
+ <tile id="11">
+  <image source="../html/maps/default/Path_to_Cave_City.webp" width="1760" height="992"/>
+ </tile>
+ <tile id="12">
+  <image source="../html/maps/default/Portal_Room.webp" width="1344" height="783"/>
+ </tile>
+ <tile id="13">
+  <image source="../html/maps/default/Reaper_boss.webp" width="1280" height="640"/>
+ </tile>
+ <tile id="14">
+  <image source="../html/maps/default/ROOT_TEST.webp" width="1280" height="714"/>
+ </tile>
+ <tile id="15">
+  <image source="../html/maps/default/Smith_in_spawn_Town.webp" width="704" height="463"/>
+ </tile>
+ <tile id="16">
+  <image source="../html/maps/default/Snake_map.webp" width="1440" height="826"/>
+ </tile>
+ <tile id="17">
+  <image source="../html/maps/default/Somewhere_small_(boss_map).webp" width="652" height="551"/>
+ </tile>
+ <tile id="18">
+  <image source="../html/maps/default/South_of_jab_stab.webp" width="1632" height="943"/>
+ </tile>
+ <tile id="19">
+  <image source="../html/maps/default/stab_jab_forest.webp" width="2567" height="1535"/>
+ </tile>
+ <tile id="20">
+  <image source="../html/maps/default/StartingTown.webp" width="1920" height="976"/>
+ </tile>
+ <tile id="21">
+  <image source="../html/maps/default/StartingTownBank.webp" width="640" height="431"/>
+ </tile>
+ <tile id="22">
+  <image source="../html/maps/default/Threnn.webp" width="1440" height="777"/>
+ </tile>
+ <tile id="23">
+  <image source="../html/maps/default/Tutorial.webp" width="1280" height="640"/>
+ </tile>
+ <tile id="24">
+  <image source="../html/maps/default/twilight_zone.webp" width="1280" height="751"/>
+ </tile>
+ <tile id="25">
+  <image source="../html/maps/default/Under_Corn_Field.webp" width="1120" height="560"/>
+ </tile>
+ <tile id="26">
+  <image source="../html/maps/default/Under_Guild_Hall.webp" width="512" height="367"/>
+ </tile>
+ <tile id="27">
+  <image source="../html/maps/default/West_of_Town.webp" width="1280" height="863"/>
+ </tile>
+ <tile id="28">
+  <image source="../html/maps/default/Winter_Town.webp" width="2578" height="1434"/>
+ </tile>
+ <tile id="29">
+  <image source="../html/maps/default/Winter_Town_General_Shop.webp" width="512" height="367"/>
+ </tile>
+ <tile id="30">
+  <image source="../html/maps/default/Winter_Wonderland.webp" width="1920" height="1098"/>
+ </tile>
+ <tile id="31">
+  <image source="../html/maps/default/Winter_Wonderland_Boss.webp" width="960" height="592"/>
+ </tile>
+ <tile id="32">
+  <image source="../html/maps/default/winter_wonderland_house.webp" width="576" height="399"/>
+ </tile>
+ <tile id="33">
+  <image source="../html/maps/default/Adexxin-armor.webp" width="640" height="431"/>
+ </tile>
+ <tile id="34">
+  <image source="../html/maps/default/Adexxin-bnb.webp" width="640" height="399"/>
+ </tile>
+ <tile id="35">
+  <image source="../html/maps/default/Adexxin-bnb-upstairs.webp" width="640" height="362"/>
+ </tile>
+ <tile id="36">
+  <image source="../html/maps/default/Adexxin-Casino.webp" width="1280" height="719"/>
+ </tile>
+ <tile id="37">
+  <image source="../html/maps/default/Adexxin-castle=stairs.webp" width="960" height="591"/>
+ </tile>
+ <tile id="38">
+  <image source="../html/maps/default/Adexxin-castle-garden.webp" width="1282" height="761"/>
+ </tile>
+ <tile id="39">
+  <image source="../html/maps/default/Adexxin-castle-garden2.webp" width="1600" height="826"/>
+ </tile>
+ <tile id="40">
+  <image source="../html/maps/default/Adexxin-castle-garden3.webp" width="640" height="502"/>
+ </tile>
+ <tile id="41">
+  <image source="../html/maps/default/Adexxin-castle-hall.webp" width="1280" height="660"/>
+ </tile>
+ <tile id="42">
+  <image source="../html/maps/default/Adexxin-Castle-main.webp" width="1280" height="687"/>
+ </tile>
+ <tile id="43">
+  <image source="../html/maps/default/Adexxin-Castle-outdoor.webp" width="1280" height="703"/>
+ </tile>
+ <tile id="44">
+  <image source="../html/maps/default/Adexxin-ChillZone.webp" width="1280" height="640"/>
+ </tile>
+ <tile id="45">
+  <image source="../html/maps/default/Adexxin-church.webp" width="1920" height="977"/>
+ </tile>
+ <tile id="46">
+  <image source="../html/maps/default/Adexxin-Darkside.webp" width="1280" height="687"/>
+ </tile>
+ <tile id="47">
+  <image source="../html/maps/default/adexxin-Darkside-northpath.webp" width="960" height="574"/>
+ </tile>
+ <tile id="48">
+  <image source="../html/maps/default/Adexxin-fightmap.webp" width="1280" height="672"/>
+ </tile>
+ <tile id="49">
+  <image source="../html/maps/default/Adexxin-gate.webp" width="1920" height="1385"/>
+ </tile>
+ <tile id="50">
+  <image source="../html/maps/default/Adexxin-Hidden.webp" width="1280" height="640"/>
+ </tile>
+ <tile id="51">
+  <image source="../html/maps/default/Adexxin-ice.webp" width="1280" height="772"/>
+ </tile>
+ <tile id="52">
+  <image source="../html/maps/default/Adexxin-Jungle.webp" width="2560" height="1280"/>
+ </tile>
+ <tile id="53">
+  <image source="../html/maps/default/Adexxin-jungle2.webp" width="1951" height="1048"/>
+ </tile>
+ <tile id="54">
+  <image source="../html/maps/default/Adexxin-jungle-hut.webp" width="1280" height="640"/>
+ </tile>
+ <tile id="55">
+  <image source="../html/maps/default/Adexxin-New.webp" width="2560" height="1466"/>
+ </tile>
+ <tile id="56">
+  <image source="../html/maps/default/Adexxin-newtown-house.webp" width="1280" height="640"/>
+ </tile>
+ <tile id="57">
+  <image source="../html/maps/default/Adexxin-Optic.webp" width="1920" height="1024"/>
+ </tile>
+ <tile id="58">
+  <image source="../html/maps/default/Adexxin-Pvp.webp" width="1280" height="640"/>
+ </tile>
+ <tile id="59">
+  <image source="../html/maps/default/Adexxin-smith.webp" width="640" height="431"/>
+ </tile>
+ <tile id="60">
+  <image source="../html/maps/default/Adexxin-start.webp" width="993" height="632"/>
+ </tile>
+ <tile id="61">
+  <image source="../html/maps/default/Adexxin-town.webp" width="1920" height="1095"/>
+ </tile>
+ <tile id="62">
+  <image source="../html/maps/default/bcave.webp" width="1984" height="1162"/>
+ </tile>
+ <tile id="63">
+  <image source="../html/maps/default/bcavebs.webp" width="1536" height="864"/>
+ </tile>
+ <tile id="64">
+  <image source="../html/maps/default/Beach_House.webp" width="1920" height="996"/>
+ </tile>
+ <tile id="65">
+  <image source="../html/maps/default/Cape_store.webp" width="512" height="368"/>
+ </tile>
+ <tile id="66">
+  <image source="../html/maps/default/Cave_Exit_For_Winter_Town.webp" width="1280" height="714"/>
+ </tile>
+ <tile id="67">
+  <image source="../html/maps/default/Cave_Map_-_Level_2.5.webp" width="1920" height="1007"/>
+ </tile>
+ <tile id="68">
+  <image source="../html/maps/default/Cave_Map_-_Level_2.webp" width="1440" height="799"/>
+ </tile>
+ <tile id="69">
+  <image source="../html/maps/default/Cave_to_Winter_Town.webp" width="1920" height="1071"/>
+ </tile>
+ <tile id="70">
+  <image source="../html/maps/default/Corn_Fields.webp" width="2560" height="1415"/>
+ </tile>
+ <tile id="71">
+  <image source="../html/maps/default/Desert_Abandoned_Building.webp" width="800" height="512"/>
+ </tile>
+ <tile id="72">
+  <image source="../html/maps/default/Desert_gear_Shop.webp" width="800" height="400"/>
+ </tile>
+ <tile id="73">
+  <image source="../html/maps/default/Desert_map.webp" width="1920" height="1183"/>
+ </tile>
+ <tile id="74">
+  <image source="../html/maps/default/Desert_Medicine_Store.webp" width="800" height="400"/>
+ </tile>
+ <tile id="75">
+  <image source="../html/maps/default/East_Caves_lv_1.webp" width="1760" height="1138"/>
+ </tile>
+ <tile id="76">
+  <image source="../html/maps/default/East_Caves_lv_2.webp" width="960" height="591"/>
+ </tile>
+ <tile id="77">
+  <image source="../html/maps/default/East_Caves_lv_3.webp" width="1920" height="1023"/>
+ </tile>
+ <tile id="78">
+  <image source="../html/maps/default/East_Guard_Break_Room.webp" width="1280" height="751"/>
+ </tile>
+ <tile id="79">
+  <image source="../html/maps/default/East_of_town.webp" width="1676" height="994"/>
+ </tile>
+ <tile id="80">
+  <image source="../html/maps/default/East_of_town_Desert.webp" width="4736" height="2368"/>
+ </tile>
+ <tile id="81">
+  <image source="../html/maps/default/Feo_Jera_Underground_City.webp" width="2880" height="1519"/>
+ </tile>
+ <tile id="82">
+  <image source="../html/maps/default/Forest_house.webp" width="640" height="431"/>
+ </tile>
+ <tile id="83">
+  <image source="../html/maps/default/Forest_map.webp" width="1920" height="1078"/>
+ </tile>
+ <tile id="84">
+  <image source="../html/maps/default/Goblin_Cave_Start.webp" width="1280" height="722"/>
+ </tile>
+ <tile id="85">
+  <image source="../html/maps/default/Goblin_Caves_-_Level_1.webp" width="1920" height="1039"/>
+ </tile>
+ <tile id="86">
+  <image source="../html/maps/default/graveyard.webp" width="1920" height="1017"/>
+ </tile>
+ <tile id="87">
+  <image source="../html/maps/default/Hank_Adventurers_Guild_in_Mistwood.webp" width="1280" height="848"/>
+ </tile>
+ <tile id="88">
+  <image source="../html/maps/default/Hank_Mistwood_Sewers.webp" width="3840" height="1920"/>
+ </tile>
+ <tile id="89">
+  <image source="../html/maps/default/Hank_Mistwood_Village.webp" width="6400" height="3496"/>
+ </tile>
+ <tile id="90">
+  <image source="../html/maps/default/Hank_Potions_and_Magic_shop_in_Mistwood.webp" width="1920" height="1053"/>
+ </tile>
+ <tile id="91">
+  <image source="../html/maps/default/Hat_shop_map.webp" width="416" height="320"/>
+ </tile>
+ <tile id="92">
+  <image source="../html/maps/default/Magic_Shop_in_Modern_Town.webp" width="1920" height="1066"/>
+ </tile>
+</tileset>
