d2774f95c318db1d3b378387ad10ad104117f776
Sani <nitori@chireiden.net>
2026-04-11 17:48:51 +0200

    Undo removal of generator

--- src/noxious_map/generator/__init__.py
+++ src/noxious_map/generator/__init__.py
@@ -1,3 +1,3 @@
 from .base import BaseGenerator as BaseGenerator
 from .mobs import MobGenerator as MobGenerator
-#from .maps import MapGenerator as MapGenerator
+from .maps import MapGenerator as MapGenerator
